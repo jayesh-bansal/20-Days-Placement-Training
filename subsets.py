25a1ecfc181c31bb6d0320f4cc4d7124f4536a14
Jayesh Bansal <jayeshbansal497@gmail.com>
2025-05-15 13:44:41 +0530

    initial commit

--- subsets.py
+++ subsets.py
@@ -1,6 +1,18 @@
+# Create a Subset
+# l = list(map(int,input().split()))
+# def subset(l,i,res):
+#     if i == len(l):
+#         return [res]
+#     return subset(l,i+1,res+[l[i]]) + subset(l,i+1,res)
+# print(subset(l,0,[]))
+
+#subset sum is equal to k
 l = list(map(int,input().split()))
-def subset(l,i,res):
-    if i == len(l):
-        return [res]
-    return subset(l,i+1,res+[l[i]]) + subset(l,i+1,res)
-print(subset(l,0,[]))
+target = int(input())
+def subset(l,i,s,target):
+    if s == target:
+        return True
+    if i == len(l) or s > target:
+        return False
+    return subset(l,i+1,s + l[i],target) or subset(l,i+1,s,target)
+print(subset(l,0,0,target))
